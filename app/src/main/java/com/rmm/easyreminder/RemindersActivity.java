b5b53fd075ff3b70c9cf0c02150f83248369e64a
Roberto <robertomdam@gmail.com>
2020-07-08 22:35:14 +0200

    First Commit.

---
+++ app/src/main/java/com/rmm/easyreminder/RemindersActivity.java
@@ -0,0 +1,14 @@
+package com.rmm.easyreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RemindersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders);
+    }
+}
