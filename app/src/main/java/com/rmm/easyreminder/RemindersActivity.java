2ed9a13bac956f2d3f2f679f5980b970e3546ebc
Roberto <robertomdam@gmail.com>
2020-07-10 23:40:52 +0200

    · Reorganization of RemindersActivity code. · Added a floating action button to add reminders. · Implemented alert dialog that gets input from the user.

--- app/src/main/java/com/rmm/easyreminder/RemindersActivity.java
+++ app/src/main/java/com/rmm/easyreminder/RemindersActivity.java
@@ -1,13 +1,18 @@
 package com.rmm.easyreminder;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.LinearLayout;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -19,11 +24,28 @@ public class RemindersActivity extends AppCompatActivity {
     RemindersRecyclerViewAdapter mRemindersRecyclerViewAdapter;
     RecyclerView.LayoutManager mLayoutManager;
 
+    AlertDialog mAlertDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminders);
 
+        initRemindersData();
+        initRemindersAdapter();
+        initFloatingActionButton();
+        initDialogInputReminder();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        refreshData();
+    }
+
+    void initRemindersData ()
+    {
         mReminders = new ArrayList<Reminder>();
 
         // Three reminders are created at the begining for testing purposes.
@@ -32,7 +54,9 @@ public class RemindersActivity extends AppCompatActivity {
         }
 
         mReminders.add( new Reminder("Test Other") );
-
+    }
+    void initRemindersAdapter ()
+    {
         mRemindersRecyclerViewAdapter = new RemindersRecyclerViewAdapter (mReminders);
         mLayoutManager = new LinearLayoutManager (this, LinearLayoutManager.VERTICAL, false);
 
@@ -41,5 +65,65 @@ public class RemindersActivity extends AppCompatActivity {
         rv_reminders.setAdapter(mRemindersRecyclerViewAdapter);
 
 //        rv_reminders.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+    }
+
+    void initFloatingActionButton()
+    {
+        findViewById (R.id.fab_reminders).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialogInputReminder();
+            }
+        });
+    }
+
+    void initDialogInputReminder()
+    {
+        final AlertDialog.Builder dialogBuilder = new AlertDialog.Builder (this);
+        dialogBuilder.setCancelable(true);
+
+        final View dialogView = getLayoutInflater().inflate(R.layout.dialog_input_reminder, null);
+        dialogBuilder.setView(dialogView);
+
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        dialogBuilder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                EditText et = dialogView.findViewById(R.id.et_input_note);
+                addReminder(et.getText().toString());
+            }
+        });
+
+        mAlertDialog = dialogBuilder.create();
+        mAlertDialog.setTitle ("Add reminder");
+    }
+
+    void refreshData ()
+    {
+        mRemindersRecyclerViewAdapter.notifyDataSetChanged();
+    }
+
+    void showDialogInputReminder ()
+    {
+        mAlertDialog.show();
+
+        // Clearing the edit text view text data
+        EditText et = mAlertDialog.findViewById (R.id.et_input_note);
+        if (et != null)
+            et.setText("");
+    }
+
+    void addReminder (String note)
+    {
+        mReminders.add (new Reminder(note));
+        refreshData();
+
+        Toast.makeText(this, "New reminded was added successfully", Toast.LENGTH_LONG).show();
     }
 }
